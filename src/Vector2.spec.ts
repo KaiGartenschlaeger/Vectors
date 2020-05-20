63bb57c602932a5af91fc57a1dd1b117a72f57b4
Kai <kai.gartenschlaeger@awin.com>
2020-05-20 13:29:10 +0200

    Added more test methods

--- src/Vector2.spec.ts
+++ src/Vector2.spec.ts
@@ -1,7 +1,18 @@
 import { Vector2 } from "./Vector2";
 
-describe("Vector2 tests", () => {
-    it("add", () => {
+describe('"Vector2 subtract', () => {
+    it("should be correct", () => {
+        let v1 = new Vector2(10, 10);
+        let v2 = new Vector2(20, 10);
+        let v3 = Vector2.subtract(v1, v2);
+
+        expect(v3.x).toBe(-10);
+        expect(v3.y).toBe(0);
+    });
+});
+
+describe("Vector2 add", () => {
+    it("should be correct", () => {
         let v1 = new Vector2(10, 10);
         let v2 = new Vector2(10, 15);
         let v3 = Vector2.add(v1, v2);
@@ -9,13 +20,24 @@ describe("Vector2 tests", () => {
         expect(v3.x).toBe(20);
         expect(v3.y).toBe(25);
     });
+});
 
-    it("subtract", () => {
+describe("Vector2 distanceTo", () => {
+    it("should be correct on same Y-Position", () => {
         let v1 = new Vector2(10, 10);
         let v2 = new Vector2(20, 10);
-        let v3 = Vector2.subtract(v1, v2);
 
-        expect(v3.x).toBe(-10);
-        expect(v3.y).toBe(0);
+        let distance = v1.distanceTo(v2);
+
+        expect(distance).toBe(10);
+    });
+
+    it("should be correct by different Y-Position", () => {
+        let v1 = new Vector2(10, 10);
+        let v2 = new Vector2(20, 20);
+
+        let distance = v1.distanceTo(v2);
+
+        expect(distance).toBeCloseTo(14.14);
     });
 });
--- src/helpers/reporter.ts
+++ src/helpers/reporter.ts
@@ -10,8 +10,19 @@ class CustomProcessor extends DisplayProcessor {
 jasmine.getEnv().clearReporters();
 jasmine.getEnv().addReporter(
     new SpecReporter({
+        suite: {
+            displayNumber: false,
+        },
+        summary: {
+            displayDuration: true,
+        },
         spec: {
+            displayDuration: false,
+            displayPending: true,
+            displayErrorMessages: true,
             displayStacktrace: StacktraceOption.PRETTY,
+            displaySuccessful: true,
+            displayFailed: true,
         },
         customProcessors: [CustomProcessor],
     })
