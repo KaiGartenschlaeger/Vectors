a2056fd5279b13d512a05c474a08371d6d3b9af8
Kai <kai.gartenschlaeger@awin.com>
2020-05-20 13:03:04 +0200

    Changed tsconfig to ES2018

--- src/helpers/reporter.ts
+++ src/helpers/reporter.ts
@@ -1,11 +1,8 @@
 import { DisplayProcessor, SpecReporter, StacktraceOption } from "jasmine-spec-reporter";
 import SuiteInfo = jasmine.SuiteInfo;
 
-console.log("jasmine-spec-reporter 1");
-
 class CustomProcessor extends DisplayProcessor {
     public displayJasmineStarted(info: SuiteInfo, log: string): string {
-        console.log("jasmine-spec-reporter 2");
         return `TypeScript ${log}`;
     }
 }
